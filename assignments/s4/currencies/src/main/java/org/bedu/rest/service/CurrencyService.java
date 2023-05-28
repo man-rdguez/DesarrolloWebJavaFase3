4c58f0b0b4466a9979a40e8da9f9a0cedcfd1ec9
man-rdguezr <man.rdguezr@gmail.com>
2023-05-28 09:30:08 -0600

    Code cleaning

--- assignments/s4/currencies/src/main/java/org/bedu/rest/service/CurrencyService.java
+++ assignments/s4/currencies/src/main/java/org/bedu/rest/service/CurrencyService.java
@@ -19,50 +19,8 @@ public class CurrencyService {
     currency.add( new Currency( "Euro","EUR") );
   }
 
-  /*
-  public boolean exists(String name) {
-    return currency.containsKey(name.toLowerCase());
-  }
-  */
-
   public List<String> getCurrencies() {
     return currency.stream().map( curr -> curr.getCode() ).collect(Collectors.toList());
   }
 
-  /*
-  public Currency getOne(String name) {
-    if (!exists(name)) {
-      throw new ContactNotFoundException(name);
-    }
-
-    return currency.get(name.toLowerCase());
-  }
-
-  public Currency add(Currency currency) {
-    if (exists(currency.getName())) {
-      throw new ContactAlreadyExistsException();
-    }
-
-    currency.put(currency.getName().toLowerCase(), currency);
-    return currency;
-  }
-
-  public void update(String name, UpdatedContact contact) {
-    if (!exists(name)) {
-      throw new ContactNotFoundException(name);
-    }
-
-    Currency current = currency.get(name);
-    current.setNumber(contact.getNumber());
-  }
-
-  public void remove(String name) {
-    if (!exists(name)) {
-      throw new ContactNotFoundException(name);
-    }
-
-    currency.remove(name);
-  }
-
-  */
 }
